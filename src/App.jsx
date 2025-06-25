e761fc703ac8881ab858fdc47e1a22ebe58d10af
MDevHub <musbaudeenabdulrahman040@gmail.com>
2025-06-25 22:03:48 +0100

    Installed React Router and set up app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-		<>
-			<h1 class="text-3xl font-bold underline">
-				Hello world!
-			</h1>
-		</>
-	)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
